c0a4d630ad8afedf0f50ab7e2bb60ba6475295be
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-03-23 12:34:55 -0600

    Update macros.asm
    Se crea una macro para poder imprimir la matriz del tablero del juego quixo.

--- macros.asm
+++ macros.asm
@@ -12,3 +12,35 @@ Cls macro   ;Macro para limpiar la pantalla
   mov  al, 3
   int  10H
 endm
+PrintRow macro len, x, o, y, null,row,ln, enter
+  LOCAL DO, VERX, VERO, VERNULL, FIN, COMPARAR
+  PrintText y
+  PUSH SI ;Registro de inice de origen, almacena desplazamiento del operando de origen en memoria en algunos tipos de operaciones 
+  PUSH AX
+  XOR SI,SI
+  DO:
+    mov al,[row+SI]
+    cmp al,0001b
+    je VERX
+    cmp al,100b
+    je VERO
+    jmp VERNULL
+  COMPARAR:
+    inc SI
+    cmp SI,len
+    jb DO
+    jmp FIN
+  VERX:
+    PrintText x    
+    jmp COMPARAR
+  VERO:
+    PrintText o 
+    jmp COMPARAR
+  VERNULL:
+    PrintText null 
+    jmp COMPARAR
+  FIN:
+    POP AX
+    POP SI
+    PrintText enter
+endm
