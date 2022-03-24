330b1eaeea460eb63773168af62639dc2d93708d
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-03-23 19:12:12 -0600

    Update macros.asm
    Se crea macro para iniciar el juego la cual crea dos numeros aleatorios de los cuales se realiza sorteo de quien empieza y de figura a utilziar. posteriormente se imprime el tablero vacio.

--- macros.asm
+++ macros.asm
@@ -27,7 +27,7 @@ PrintRow macro len, x, o, y, null,row,ln, enter
     jmp VERNULL
   COMPARAR:
     inc SI
-    cmp SI,len
+    cmp SI,SIZEOF len
     jb DO
     jmp FIN
   VERX:
@@ -43,4 +43,98 @@ PrintRow macro len, x, o, y, null,row,ln, enter
     POP AX
     POP SI
     PrintText enter
+
+endm
+
+IniciarJuego macro
+    PrintText prueba
+    PrintText msjPrueba
+    numAleatorio1
+    mov ah, 02h
+    mov dl, numA1
+    add dl, '0'
+    int 21h
+    case11:
+        cmp numA1,1
+        jne case22
+        sorteo2
+        jmp next
+    case22:
+        cmp numA1,0
+        sorteo1
+        jmp next
+    next:
+      PrintText msjPrueba2
+      numAleatorio2
+      mov ah, 02h
+      mov dl, numA2
+      add dl, '0'
+      int 21h
+      jmp case111
+    case111:
+        cmp numA2,0
+        jne case222
+        turno0
+        jmp FIN1
+    case222:
+        cmp numA2,1
+        turno1
+        jmp FIN1
+    FIN1:
+        PrintText FINX
+        PrintText welc
+        PrintRow row1,x,o,ye,null,row1,ln,saltoLinea
+        PrintRow row2,x,o,y4,null,row2,ln,saltoLinea
+		    PrintRow row3,x,o,yc,null,row3,ln,saltoLinea
+		    PrintRow row4,x,o,y2,null,row4,ln,saltoLinea
+		    PrintRow row5,x,o,ya,null,row5,ln,saltoLinea
+        ;PrintText ln
+	     	PrintText xtitles
+	    	PrintText stop
+endm
+
+CargarJuego macro
+    PrintText prueba2
+endm
+
+sorteo1 macro
+    PrintText pieza1x
+    PrintText pieza1O
+endm
+
+sorteo2 macro
+    PrintText pieza2O
+    PrintText pieza2x
+endm
+
+turno0 macro
+    PrintText turno11
+    PrintText turno12
+endm
+
+turno1 macro
+    PrintText turno21
+    PrintText turno22
+endm
+
+numAleatorio1 macro
+    mov ah, 0h ;interrupciones para obtener la hora del sistema.
+    int 1ah    ;ahora no se guardará ningún reloj en dx
+
+    mov ax, dx ;mover no de relojes marcan en ax
+    mov dx, 0  ;borrar el dx a cero
+    mov bx,2  ;bx=2 nuestro divisor para generar no entre 0 y 1 como resto.
+    div bx     ;divida ax por bx, sea ax=152, bx=2, luego dx=0
+    mov numA1, dl  ;obtenga el divisor de dl y guárdelo en la variable randomNum.
+endm
+
+numAleatorio2 macro
+    mov ah, 0h ;interrupciones para obtener la hora del sistema.
+    int 1ah    ;ahora no se guardará ningún reloj en dx
+
+    mov ax, dx ;mover no de relojes marcan en ax
+    mov dx, 0  ;borrar el dx a cero
+    mov bx,2  ;bx=2 nuestro divisor para generar no entre 0 y 1 como resto.
+    div bx     ;divida ax por bx, sea ax=152, bx=2, luego dx=0
+    mov numA2, dl  ;obtenga el divisor de dl y guárdelo en la variable randomNum.
 endm
