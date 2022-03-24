ef402192000fa73ce1d4354aa8bb0942fc76fb10
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-03-24 16:21:17 -0600

    Update macros.asm
    Lista de cambios realizados:
    
    Se agrego una macro para que despues del mensaje de inicio se presione enter para continuar.
    
    En las macros de sorteos y turnos se movio informacion hacia variables para poder hacer las impresiones .
    
    Se realizaron diferentes macros utilizadas en el comando SAVE para poder crear archivos, abirlos, escribir sobre ellos la informacion  y posteiormente  cerrarlo.

--- macros.asm
+++ macros.asm
@@ -88,9 +88,15 @@ IniciarJuego macro
 		    PrintRow row3,x,o,yc,null,row3,ln,saltoLinea
 		    PrintRow row4,x,o,y2,null,row4,ln,saltoLinea
 		    PrintRow row5,x,o,ya,null,row5,ln,saltoLinea
-        ;PrintText ln
 	     	PrintText xtitles
 	    	PrintText stop
+        
+
+endm
+
+PressEnter macro
+  mov ah, 00
+  int 16h
 endm
 
 CargarJuego macro
@@ -98,21 +104,25 @@ CargarJuego macro
 endm
 
 sorteo1 macro
+    mov fig,0b ;0b representara que para el jugador 1 sera la X
     PrintText pieza1x
     PrintText pieza1O
 endm
 
 sorteo2 macro
+    mov fig,1b ;1b representara que para el jugador 2 sera la X
     PrintText pieza2O
     PrintText pieza2x
 endm
 
 turno0 macro
+    mov turno,0b ;0b significara que el jugador 1 sera el primero en jugar.
     PrintText turno11
     PrintText turno12
 endm
 
 turno1 macro
+    mov turno,1b ;1b significara que el jugador 2 sera el primero en jugar.
     PrintText turno21
     PrintText turno22
 endm
@@ -129,6 +139,7 @@ numAleatorio1 macro
 endm
 
 numAleatorio2 macro
+
     mov ah, 0h ;interrupciones para obtener la hora del sistema.
     int 1ah    ;ahora no se guardará ningún reloj en dx
 
@@ -137,4 +148,122 @@ numAleatorio2 macro
     mov bx,2  ;bx=2 nuestro divisor para generar no entre 0 y 1 como resto.
     div bx     ;divida ax por bx, sea ax=152, bx=2, luego dx=0
     mov numA2, dl  ;obtenga el divisor de dl y guárdelo en la variable randomNum.
+endm
+
+;MACROS PARA COMANDOS
+
+GetText macro buffer
+  LOCAL CONTINUE, FIN
+  PUSH SI
+  PUSH AX
+  xor si, si
+  CONTINUE:
+    getChar
+    cmp al, 0dh
+    je FIN
+    mov buffer[si], al
+    inc si
+    jmp CONTINUE
+  FIN:
+    mov al, '$'
+    mov buffer[si], al
+  POP AX
+  POP SI
+endm
+
+getChar macro
+		mov ah, 01h
+		int 21h
+	endm
+
+getRuta macro buffer
+    LOCAL CONTINUE, FIN
+    PUSH SI
+    PUSH AX
+
+    xor si,si
+    CONTINUE:
+    getChar
+    cmp al,0dh
+    je FIN
+    mov buffer[si],al
+    inc si
+    jmp CONTINUE
+
+    FIN:
+    mov al,'$'
+    mov buffer[si],al
+    POP AX
+    POP SI
+endm
+generarCarga macro rutaArchivo, handle
+    crearArchivo rutaArchivo, handle
+    abrirArchivo rutaArchivo, handle
+    generarCargaFila row1,handle
+    generarCargaFila row2,handle
+    generarCargaFila row3,handle
+    generarCargaFila row4,handle
+    generarCargaFila row5,handle
+    cerrarArchivo handle
+endm
+
+
+crearArchivo macro buffer,handle
+    mov ah,3ch
+    mov cx,00h
+    lea dx,buffer
+    int 21h
+    mov handle,ax
+     jc ErrorCrear
+endm
+abrirArchivo macro ruta,handle
+  
+    mov ah,3dh
+    mov al,10b
+    lea dx,ruta
+    int 21h
+    mov handle,ax
+    jc ErrorAbrir
+endm
+
+generarCargaFila macro arreglo, handle
+LOCAL CICLO, printX,printO,Afuera
+    mov cx,5
+    xor si,si
+    CICLO: 
+        cmp arreglo[si],001b 
+        je printX
+        cmp arreglo[si],100b
+        je printO
+        escribirArchivo  carganull, carganull, handle
+        jmp Afuera
+        printX:
+            escribirArchivo  cargax, cargax, handle
+            jmp Afuera
+        printO:
+            escribirArchivo  cargao, cargao, handle   
+            jmp Afuera   
+        Afuera:
+        
+        inc si
+        dec cx
+    JNE CICLO
+endm
+cerrarArchivo macro handle
+    mov ah,3eh
+    mov handle,bx
+    int 21h
+endm
+escribirArchivo macro numbytes,buffer,handle
+    push cx
+    escribir  numbytes,buffer,handle
+    pop cx
+endm
+escribir macro numbytes,buffer,handle
+	mov ah, 40h
+	mov bx,handle
+	mov cx, SIZEOF numbytes
+	lea dx,buffer
+	int 21h
+	jc ErrorEscribir
 endm
